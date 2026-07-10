36ca077173ed3fe495027f45a571485d7532b2a0
Ritik13 <critik.singh@gmail.com>
2026-07-10 20:29:14 +0530

    add: tests folder

---
+++ conftest.py
@@ -0,0 +1,19 @@
+# tests/conftest.py
+import pytest
+import asyncio
+from database import engine
+from models import Base
+
+@pytest.fixture(scope="session", autouse=True)
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+@pytest.fixture(scope="session", autouse=True)
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
