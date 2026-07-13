cba0fff378ac8a280c98ce64db7739f0c126a7d7
Ritik13 <critik.singh@gmail.com>
2026-07-13 14:57:20 +0530

    add: conftest

--- conftest.py
+++ conftest.py
@@ -4,17 +4,20 @@ import asyncio
 from database import engine
 from models import Base
 
-pytest_plugins = ['pytest_asyncio']
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session")
 def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
+    loop = asyncio.new_event_loop()
     yield loop
     loop.close()
 
 @pytest.fixture(scope="session", autouse=True)
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+def create_tables(event_loop):
+    async def _create():
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+    event_loop.run_until_complete(_create())
     yield
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+    async def _drop():
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+    event_loop.run_until_complete(_drop())
